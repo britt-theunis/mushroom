bc0639edc2a9e809efad0f095c77559cc30fd38b
britt-theunis <r0785522@student.thomasmore.be>
2021-12-12 23:14:36 +0100

    deploy niet werkende app

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os
 from flask import Flask, request, make_response, jsonify, render_template, redirect, flash
 from flask.helpers import url_for
 from werkzeug.utils import secure_filename
-from fastai.vision.all import *
-from fastai.data.external import *
+#from fastai.vision.all import *
+#from fastai.data.external import *
 
 
 # codeblock below is needed for Windows path #############
@@ -20,9 +20,9 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 
 # loading the pickle files from our 3 models
-learner1 = load_learner('mushroom_noMushroom.pkl')
-learner2 = load_learner('toxic_safe.pkl')
-learner3 = load_learner('species.pkl')
+#learner1 = load_learner('mushroom_noMushroom.pkl')
+#learner2 = load_learner('toxic_safe.pkl')
+#learner3 = load_learner('species.pkl')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -50,37 +50,37 @@ def upload_file():
 
             file.save('./static/' + filename)
 
-            img = PILImage.create(file)
+           # img = PILImage.create(file)
             
             # prediction whether it is a mushroom or not
-            pred1 = learner1.predict(img)
+           # pred1 = learner1.predict(img)
 
             # if it is a mushroom
-            if pred1[0] == "mushroom":
+           # if pred1[0] == "mushroom":
                 # invoke models
                 # accuracy (in %) that the model knows it is indeed a mushroom
-                perc1 = round(float(pred1[2][0])*100, 2)
+              #  perc1 = round(float(pred1[2][0])*100, 2)
 
                 # prediction of whether the mushroom is edible or not
-                pred2 = learner2.predict(img)
+              # pred2 = learner2.predict(img)
                 # accuracy (in %) that the model knows that the mushroom is edible or not
-                perc2 = round(float(max(pred2[2]))*100, 2)
+               # perc2 = round(float(max(pred2[2]))*100, 2)
                 
                 # prediction of the mushroom species
-                pred3 = learner3.predict(img)
+              #  pred3 = learner3.predict(img)
                 # accuracy (in %) that the model knows what the mushroom species is
-                perc3 = round(float(max(pred3[2]))*100, 2)
+              #  perc3 = round(float(max(pred3[2]))*100, 2)
 
                 # go to the resultGood page with the variables defined above
-                return render_template("resultGood.html", 
-                filename=filename, 
-                perc1 = perc1,
-                variable2 = pred2[0], perc2 = perc2,
-                variable3 = pred3[0], perc3 = perc3)
+               # return render_template("resultGood.html", 
+               # filename=filename, 
+               # perc1 = perc1,
+              #  variable2 = pred2[0], perc2 = perc2,
+             #   variable3 = pred3[0], perc3 = perc3)
 
             # if it is not a mushroom
-            if pred1[0] == "noMushroom":
+            #if pred1[0] == "noMushroom":
                  # go to the resultFalse page
-                return render_template("resultFalse.html", filename = filename)
+               # return render_template("resultFalse.html", filename = filename)
 
     return render_template('home.html')
